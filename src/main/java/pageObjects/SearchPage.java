4ab3101465d9b6b2ee3cc2355f2c16a7b6cd6ca6
tburto33 <44762286+tburto33@users.noreply.github.com>
2020-06-21 14:41:50 -0400

    Added random search term array, added keyboard enter key bot, added clear search bar and enter new random search method

--- src/main/java/pageObjects/SearchPage.java
+++ src/main/java/pageObjects/SearchPage.java
@@ -3,7 +3,14 @@ package pageObjects;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class SearchPage {
+
 
     @FindBy(id = "search_form_input_homepage")
     private WebElement inputSearch;
@@ -13,9 +20,36 @@ public class SearchPage {
     private WebElement containerSearchResults;
     @FindBy(xpath = "//div[contains(@class, 'js-images-thumbnails')]")
     private WebElement pictureContainerSearchResults;
+    @FindBy(id = "search_form")
+    private WebElement searchResultsPageSearchBar;
+
+    //List of search terms
+    String[] searchTerms = {
+            "Baby Ferret Pictures",
+            "Home Gardening",
+            "How to hit immortal DOTA2",
+            "Stackoverflow helped me again",
+            "Bearded Dragon Pictures"
+    };
+
+    //Selects random search term to insert into search bar.
+    Random randomSearchTerm = new Random();
+    int select = randomSearchTerm.nextInt(searchTerms.length);
+
+    //Bot for clicking simulated keys on keyboard.
+    Robot robot = new Robot();
+
+    public SearchPage() throws AWTException {
+    }
+
 
     public void fillSearchInputWithTerm() {
-        inputSearch.sendKeys("Baby Ferret Pictures");
+        inputSearch.sendKeys(searchTerms[select]);
+    }
+
+    public void clearThenEnterNewSearchTerm(){
+        searchResultsPageSearchBar.clear();
+        inputSearch.sendKeys(searchTerms[select]);
     }
 
     public void clickSearchBtn(){
@@ -34,6 +68,10 @@ public class SearchPage {
             return true;
         }
         return false;
+    }
+
+    public void keyboardEnterBtn(){
+        robot.keyPress(KeyEvent.VK_ENTER);
     }
 }
 
--- src/test/java/DDGSearchTests.java
+++ src/test/java/DDGSearchTests.java
@@ -26,12 +26,17 @@ public class DDGSearchTests {
         // Navigates to base URL.
         driver.get("http://duckduckgo.com/");
 
-        //Search with provided term, submit and assert page/picture landing.
+        //Search with provided term, submit and assert page/picture landing, search query in search results,.
         searchPage.fillSearchInputWithTerm();
         searchPage.clickSearchBtn();
         Assert.assertTrue(searchPage.isSearchResultsDisplayed(), "Verifying Search Results Container.");
         Assert.assertTrue(searchPage.isPictureResultsDisplayed(), "Verifying Picture Results Container");
 
+        //Next test, same as above with using keyboard enter key.
+        searchPage.clearThenEnterNewSearchTerm();
+        searchPage.keyboardEnterBtn();
+        Assert.assertTrue(searchPage.isSearchResultsDisplayed(), "Verifying Search Results Container.");
+        Assert.assertTrue(searchPage.isPictureResultsDisplayed(), "Verifying Picture Results Container");
 
         // Calling driver quit to close and dispose of driver and process.
         driver.quit();
